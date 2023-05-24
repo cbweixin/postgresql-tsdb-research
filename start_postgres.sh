ac8ba1c401ca0bddc8e66537ff0cf0ebdb7fe15d
cbweixin <cbweixin@qq.com>
2023-05-23 21:21:27 -0700

    new script

---
+++ start_postgres.sh
@@ -0,0 +1,9 @@
+#bin/zsh
+docker run --name my-postgresql \
+    -p 15432:5432 \
+    -e POSTGRES_PASSWORD=root \
+    -e POSTGRES_DB=test \
+    -e POSTGRES_INITDB_WALDIR=/var/lib/postgresql/log \
+    -v /Users/xinwei/docker_data/postgresql/data:/var/ltb/postgresql/data \
+    -v /Users/xinwei/docker_data/postgresql/log:/var/lib/postgresql/log \
+    postgres
